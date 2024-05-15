1204acacab599b666ec470217f237daf6c9d19ff
IlyaGenShubin <166981604+IlyaGenShubin@users.noreply.github.com>
2024-05-15 17:04:01 +0300

    Update detector.py

--- detector.py
+++ detector.py
@@ -1,1 +1,8 @@
+from ultralytics import YOLO
 
+def pred(img):  
+  model = YOLO('yolov8n.pt')
+  results = model.predict(img)
+  result = results[0]
+  box = result.boxes[0]
+  return print(result.names[box.cls[0].item()])
